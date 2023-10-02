f035aefcc19130f247f6c4b44bd050aa528edd3f
TylerTLe <tyler.le@edu.sait.ca>
2023-10-02 14:19:56 -0600

    creates cone class

---
+++ Assignment 1/src/shape/Cone.java
@@ -0,0 +1,28 @@
+package shape;
+
+public class Cone extends Shape {
+    private double radius;
+
+    public Cone(double height, double radius) {
+        super(height);
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double getVolume() {
+        return (getBaseArea() * getHeight()) / 3;
+    }
+
+    @Override
+    public double getBaseArea() {
+        return Math.PI * Math.pow(radius, 2);
+    }
+}
